3cc42996bf740a384341cfe5e286349c9b119bb4
Ansh Singh <singhansh9479@gmail.com>
2026-07-20 23:55:54 +0530

    images problem

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "images");
+        cb(null, path.join(__dirname, "../images"));
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
